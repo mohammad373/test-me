f69c69ce266c18e581f828ae0942dec639fc6c91
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2021-08-03 19:55:24 +0430

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import arp_spoof
+import not_internet_for_target
+import os
+import sys
+import time
+from baner import baner
+os.system("clear")
+baner()
+time.sleep(0.6
+print(Fore.LIGHTBLACK_EX + "\n[1] ~ ARP Spoofer")
+time.sleep(0.3)
+print(Fore.LIGHTBLACK_EX + "[2] ~ Bloack Target For Internet")
+number = int(input(Fore.LIGHTBLACK_EX + "\nPleass Enter Your Number : ")
+if number == 1:
+             arp_spoof._all_()
+if number == 2:
+             not_internet_for_target._all_()
