04c43ff409c14416ccf326a02c4d03e4f027b7d8
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2021-08-03 20:00:06 +0430

    Update main.py

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ time.sleep(0.6)
 print(Fore.LIGHTBLACK_EX + "\n[1] ~ ARP Spoofer")
 time.sleep(0.3)
 print(Fore.LIGHTBLACK_EX + "[2] ~ Bloack Target For Internet")
-number = int(input(Fore.LIGHTBLACK_EX + "\nPleass Enter Your Number : ")
+number = int(input(Fore.LIGHTBLACK_EX + "\nPleass Enter Your Number : "))
 if number == 1:
-             arp_spoof._all_()
+    arp_spoof._all_()
 if number == 2:
-             not_internet_for_target._all_()
+    not_internet_for_target._all_()
