5682e860cf7a13bba05e99b4cdd19c720a5c57d6
mohammad373 <75175976+mohammad373@users.noreply.github.com>
2021-08-03 23:31:12 +0430

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import arp_spoof
 import not_internet_for_target
+import mac_changer
 import os
 import sys
 import time
@@ -10,8 +11,16 @@ time.sleep(0.6)
 print(Fore.LIGHTBLACK_EX + "\n[1] ~ ARP Spoofer")
 time.sleep(0.3)
 print(Fore.LIGHTBLACK_EX + "[2] ~ Bloack Target For Internet")
+time.sleep(0.3)
+print(Fore.LIGHTBLACK_EX + "[3] ~ MAC Changer")
+time.sleep(1)
 number = int(input(Fore.LIGHTBLACK_EX + "\nPleass Enter Your Number : "))
 if number == 1:
     arp_spoof._all_()
 if number == 2:
     not_internet_for_target._all_()
+if number == 3:
+    mac_changer._all_()
+else:
+    print(Fore.RED + "\n[-] Your Number Not Found !!!")
+    sys.exit()
